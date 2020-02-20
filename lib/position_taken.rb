1b71108d1d5ab4cf62d2c1acc02f9830547a0668
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 13:05:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@
 
   def position_taken?(board,index)
     if (board[index] == "X" || board[index] == "O")
-    return true	   
-  else	  
-    return false	    
-  end	  
+    return true
+  else
+    return false
+  end
 
   end
