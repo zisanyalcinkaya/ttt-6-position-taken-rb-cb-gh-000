4c95d7189caab3afe327323131b1a1a1f6179c05
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 12:53:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-  def position_taken(board,index)
+  def position_taken?(board,index)
     if( board[index] == "" || nil || " ")
       return false
     end
