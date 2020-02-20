d9a5a460d8386ad966c7488a712c53697a3a2a7e
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 12:42:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   def position_taken(board,index)
-    if board[index] == " "
+    if board[index-1] == " "
       return false
     end
   end
